e2d8a8c79e39a4bf5c2d15e2a2095642be4cb07a
Thu Ta <thuta.ai3817@gmail.com>
2024-06-01 15:42:18 +0630

    update title fixed

--- gallery.py
+++ gallery.py
@@ -4,7 +4,7 @@ from streamlit_pdf_viewer import pdf_viewer
 import time
 
 main_folder_path = os.listdir("certificates")
-st.subheader("Collection of links to :green[DataCamp] certificates that I(:blue[Thu Ta]) has achieved")
+st.subheader("Collection of :green[DataCamp] certificates that I(:blue[Thu Ta]) has achieved")
 for i, f in enumerate(main_folder_path):
     st.write(f"{i+1}. {f.rstrip('.pdf')}")
     time.sleep(0.5)
